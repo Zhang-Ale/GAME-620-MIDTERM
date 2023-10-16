4aa502ed57661147c0a0a375aa7d37c243d8b4cb
HongChengZ <moonwolfzhang@gmail.com>
2023-10-16 03:49:18 -0400

    s

--- The value of friendship/Assets/_Scripts/BackgroundColor.cs
+++ The value of friendship/Assets/_Scripts/BackgroundColor.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class BackgroundColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer SR;
+    public Color[] colors; 
+    private float targetPoint;
+    private int curColInt = 0;
+    private int newColInt = 1;
+    public bool changeCol;
+    float timer = 0.0f; 
+
+    void FixedUpdate()
     {
-        
+        if (changeCol)
+        {
+            //Transition(); 
+            SmoothTransition(); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Transition()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= 3.0f)
+        {
+            Color col = new Color(Random.value, Random.value, Random.value, 1.0f);
+            SR.color = col;
+            timer = 0;
+        }
+    }
+
+    void SmoothTransition()
+    {
+        targetPoint += Time.deltaTime;
+        SR.color = Color.Lerp(colors[curColInt], colors[newColInt], targetPoint);
+        if(targetPoint >= 3f)
+        {
+            targetPoint = 0f;
+            curColInt = newColInt;
+            newColInt++;
+            if (newColInt == colors.Length)
+                newColInt = 0; 
+        }
     }
 }
